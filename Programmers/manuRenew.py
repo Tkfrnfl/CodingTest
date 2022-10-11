5d53986bb6f337157580c98671bc5baf65f20d21
jun <jun@akaai.kr>
2022-10-11 19:38:38 +0900

    save

---
+++ Programmers/manuRenew.py
@@ -0,0 +1,17 @@
+def solution(orders, course):
+    answer = []
+
+
+    def get_combi(order_list):
+        sub_list=
+
+    for idx,val in enumerate(orders):
+        for id,va in enumerate(course):
+            get_combi(val)
+    
+
+
+    return answer
+
+if __name__ == "__main__":
+	solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"],[2,3,4])         
